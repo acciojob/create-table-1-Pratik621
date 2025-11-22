17568ff83eeae75b96a6ab0221b74ccd7cc32a25
Pratik Mane <manepratik3727@gmail.com>
2025-11-23 01:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
+	const table=document.querySelector("table");
+	const newrow=document.createElement("tr")
+	const cell1=document.createElement("td")
+	const cell2=document.createElement("td")
+
+	cell1.textContent="New Cell1"
+	cell2.textContent="New Cell2"
+
+	newrow.appendChild(cell1)
+	newrow.appendChild(cell2)
+	table.prepend(newrow)
   
   
 }
