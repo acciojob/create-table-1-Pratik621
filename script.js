a137721c3f3edcff1528932089cc9321139f8ae0
Pratik Mane <manepratik3727@gmail.com>
2025-11-23 02:04:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function insert_Row() {
 	const cell1=document.createElement("td")
 	const cell2=document.createElement("td")
 
-	cell1.textContent="New cell1"
-	cell2.textContent="New cell2"
+	cell1.textContent="New Cell1"
+	cell2.textContent="New Cell2"
 
 	newrow.appendChild(cell1)
 	newrow.appendChild(cell2)
